ac73d0e5500ae370dbeb17e2c32226f271b9a2fe
Ravid Rahman <ravidrahman@gmail.com>
2023-03-03 12:07:07 -0700

    added fields, properties, and constructors for meal class

--- health_index_app/health_index_app/Shared/Meal.cs
+++ health_index_app/health_index_app/Shared/Meal.cs
@@ -8,5 +8,21 @@ namespace health_index_app.Shared
 {
     internal class Meal
     {
+        private List<Food> foodItems;
+        private double healthIndex;
+
+        public List<Food> FoodItems { get; set; }
+        public double HealthIndex { get; set; };
+
+        public Meal(List<Food> foodItems, double healthIndex)
+        {
+            this.food_items = foodItems;
+            this.healthIndex = healthIndex;
+        }
+
+        public Meal()
+        {
+
+        }
     }
 }
