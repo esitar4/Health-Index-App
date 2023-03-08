64ab1a3574e6fad165813a7ef37e0d1a430b644b
Ravid Rahman <ravidrahman@gmail.com>
2023-03-08 16:30:28 -0700

    fixed properties to match uml diagram

--- health_index_app/health_index_app/Shared/Meal.cs
+++ health_index_app/health_index_app/Shared/Meal.cs
@@ -8,16 +8,16 @@ namespace health_index_app.Shared
 {
     internal class Meal
     {
-        private List<Food> foodItems;
-        private double healthIndex;
 
         public List<Food> FoodItems { get; set; }
         public double HealthIndex { get; set; }
+        public int MealID { get; set; }
 
-        public Meal(List<Food> foodItems, double healthIndex)
+        public Meal(List<Food> FoodItems, double HealthIndex, int MealID)
         {
-            this.foodItems = foodItems;
-            this.healthIndex = healthIndex;
+            this.FoodItems = FoodItems;
+            this.HealthIndex = HealthIndex;
+            this.MealID = MealID;
         }
 
         public Meal()
