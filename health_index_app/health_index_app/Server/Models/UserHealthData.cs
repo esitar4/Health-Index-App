18253e72fdef5eadd6f1dd97112f5b0c4df90b5b
Ravid Rahman <ravidrahman@gmail.com>
2023-03-15 11:40:16 -0600

    Added annotations for Admnin class; minor changes for other annotations

--- health_index_app/health_index_app/Server/Models/UserHealthData.cs
+++ health_index_app/health_index_app/Server/Models/UserHealthData.cs
@@ -15,15 +15,15 @@ namespace health_index_app.Server.Models
         [Required]
         //[MinLength(6, ErrorMessage = "Username must be at least 6 characters")]
         //[MaxLength(15, ErrorMessage = "Username must be no longer than 15 characters")]
-        [RegularExpression(@"^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
+        [RegularExpression("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
             ErrorMessage = "Username must be between 8 and 20 characters and cannot include '_' or '.'")]
         public string Username { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        [Column(TypeName = "decimal(3,2)")]
+        [Column(TypeName = "decimal(5,2)")]
         public double? Weight { get; set; }
-        [Column(TypeName = "decimal(2,2)")]
+        [Column(TypeName = "decimal(4,2)")]
         public double? Height { get; set; }
-        [RegularExpression(@"[MFO]")]   //character for internal use - parsed from dropdown menu on frontend
+        [RegularExpression("[MFO]")]   //character for internal use - parsed from dropdown menu on frontend
         public char? Gender { get; set; }
     }
 }
