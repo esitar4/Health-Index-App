1d21d4974c82adb5ee25ff94e911a9ab0eefcbd2
Scott Li <vikash23galce@gmail.com>
2023-03-19 23:40:58 -0600

    Remove Email from ApplicationUser and database

--- health_index_app/health_index_app/Server/Models/ApplicationUser.cs
+++ health_index_app/health_index_app/Server/Models/ApplicationUser.cs
@@ -13,9 +13,9 @@ namespace health_index_app.Server.Models
         [RegularExpression("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
             ErrorMessage = "Username must be between 8 and 20 characters and cannot include '_' or '.'")]
         public string Username { get; set; }*/
-        [Required]
-        [EmailAddress(ErrorMessage = "Not a valid email address")]
-        public string Email { get; set; }
+        //[Required]
+        //[EmailAddress(ErrorMessage = "Not a valid email address")]
+        //public string Email { get; set; }
 /*        [Required]
         //[MinLength(8, ErrorMessage = "Password must be at least 6 characters")]
         //[MaxLength(10, ErrorMessage = "Password must be no longer than 15 characters")]
