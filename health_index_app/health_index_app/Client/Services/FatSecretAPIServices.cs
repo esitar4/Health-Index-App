6d40cba7d4d37b8806acd50efe2c5b3cf6979b31
Scott Li <64591509+glace23@users.noreply.github.com>
2023-03-29 11:10:21 -0600

    Add error handling for FatSecret API Services

--- health_index_app/health_index_app/Client/Services/FatSecretAPIServices.cs
+++ health_index_app/health_index_app/Client/Services/FatSecretAPIServices.cs
@@ -20,14 +20,36 @@ namespace health_index_app.Client.Services
 
         public async Task<FoodsSearchResponse> FoodsSearchAsync(string searchExpression)
         {
-            var url = $"/api/fatsecret/foodsearch?searchExpression={searchExpression}";
-            return await _client.GetFromJsonAsync<FoodsSearchResponse>(url);
+            FoodsSearchResponse response;
+            try
+            {
+                var url = $"/api/fatsecret/foodsearch?searchExpression={searchExpression}";
+                response = await _client.GetFromJsonAsync<FoodsSearchResponse>(url);
+            } catch
+            {
+                response = new FoodsSearchResponse
+                {
+                    Error = new FatSecretError { Message = "Unable to get a response!"}
+                };
+            }
+            return response;
         }
 
         public async Task<GetFoodResponse> FoodGetAsync(int foodId)
         {
-            var url = $"/api/fatsecret/foodget?foodId={foodId}";
-            return await _client.GetFromJsonAsync<GetFoodResponse>(url);
+            GetFoodResponse response;
+            try
+            {
+                var url = $"/api/fatsecret/foodget?foodId={foodId}";
+                response = await _client.GetFromJsonAsync<GetFoodResponse>(url);
+            } catch
+            {
+                response = new GetFoodResponse
+                {
+                    Error = new FatSecretError { Message = "Unable to get a response!" }
+                };
+            }
+            return response;
         }
     }
 }
