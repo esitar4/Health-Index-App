6e71dbe887811e9cd1c390f2d38a6379075d89f5
Edward Sitar <edwardsitar4@gmail.com>
2023-03-29 08:59:17 -0600

    Fields and button to update user data in settings

--- health_index_app/health_index_app/Server/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ health_index_app/health_index_app/Server/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -63,20 +63,20 @@ namespace health_index_app.Server.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Phone]
-            [Display(Name = "Phone number")]
+            [Display(Name = "Update Phone number")]
             public string PhoneNumber { get; set; }
 
             [DateOfBirth(ErrorMessage = "Date of Birth must be in the past")]
-            [Display(Name = "Date of Birth (Optional)")]
+            [Display(Name = "Date of Birth")]
             public DateTime? DateOfBirth { get; set; }
-            [Display(Name = "Weight (Optional - pounds)")]
+            [Display(Name = "Update Weight (pounds)")]
             [Range(0.001, 9999.99, ErrorMessage = "Weight must be in between 0 and 9999.99")]
             public double? Weight { get; set; }
-            [Display(Name = "Height (Optional - inches)")]
+            [Display(Name = "Update Height (inches)")]
             [Range(0.001, 999.99, ErrorMessage = "Height must be in between 0 and 999.99")]
             public double? Height { get; set; }
             [RegularExpression("[MFO]", ErrorMessage = "Invalid Gender Character")]   //character for internal use - parsed from dropdown menu on frontend
-            [Display(Name = "Gender (Optional")]
+            [Display(Name = "Gender (Optional)")]
             public char? Gender { get; set; }
         }
 
@@ -121,6 +121,15 @@ namespace health_index_app.Server.Areas.Identity.Pages.Account.Manage
                 return Page();
             }
 
+            Console.WriteLine($"Old phone number: {this.PhoneNumber} old height: {this.Height} old weight: {this.Weight}");
+            user.PhoneNumber = Input.PhoneNumber;
+            user.Weight = Input.Weight;
+            user.Height = Input.Height;
+            user.Gender = Input.Gender;
+            var updateSuccess = await _userManager.UpdateAsync(user);
+            Console.WriteLine($"new height: {user.Height} new weight: {user.Weight}");
+
+            /*
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
             {
@@ -131,6 +140,7 @@ namespace health_index_app.Server.Areas.Identity.Pages.Account.Manage
                     return RedirectToPage();
                 }
             }
+            */
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
