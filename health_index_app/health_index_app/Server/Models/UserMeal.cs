523809afe32d743ac5edb74f7b325c017a7b202e
Scott Li <64591509+glace23@users.noreply.github.com>
2023-03-29 14:53:27 -0600

    Make foreign key objects nullable

--- health_index_app/health_index_app/Server/Models/UserMeal.cs
+++ health_index_app/health_index_app/Server/Models/UserMeal.cs
@@ -14,12 +14,12 @@ namespace health_index_app.Server.Models
         [ForeignKey("ApplicationUser")]
         public string UserId { get; set; }
 
-        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual ApplicationUser? ApplicationUser { get; set; }
 
         [ForeignKey("Meal")]
         public int MealId { get; set; }
 
-        public virtual Meal Meal { get; set; }
+        public virtual Meal? Meal { get; set; }
 
         [MaxLength(15, ErrorMessage = "Name length shouldn't be longer than 15 letters")]
         [Required(ErrorMessage = "Name is a required field")]
