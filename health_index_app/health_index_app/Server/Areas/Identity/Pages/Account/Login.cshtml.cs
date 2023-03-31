bf9de764a033af54c21ed2411030a4f18ec455c3
Edward Sitar <edwardsitar4@gmail.com>
2023-03-31 08:50:32 -0600

    Add user lockout after 5 invalid login attempts

--- health_index_app/health_index_app/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ health_index_app/health_index_app/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -112,7 +112,7 @@ namespace health_index_app.Server.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
                     var user = _signInManager.UserManager.Users.Where(u => u.Email == Input.Email).FirstOrDefault();
