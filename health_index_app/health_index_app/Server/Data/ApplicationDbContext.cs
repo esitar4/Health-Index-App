95531a7a4db757e14980125e9ce46b70b8a69448
Scott Li <vikash23galce@gmail.com>
2023-04-03 19:17:43 -0600

    Add users on model create

--- health_index_app/health_index_app/Server/Data/ApplicationDbContext.cs
+++ health_index_app/health_index_app/Server/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using health_index_app.Server.Models;
 using health_index_app.Shared.Models;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -22,8 +23,79 @@ namespace health_index_app.Server.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<ApplicationUser>().HasData(new ApplicationUser { Id = "440b047a-9335-4099-8b7c-1929aac527c4", UserName = "admin@cog.com"});
-            builder.Entity<ApplicationUser>().HasData(new ApplicationUser { UserName = "child1@cog.com", ParentId = "440b047a-9335-4099-8b7c-1929aac527c4" });
+            string AdminUserId = Guid.NewGuid().ToString();
+            string AdminRoleId = Guid.NewGuid().ToString();
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Id = AdminRoleId, Name = "Admin", NormalizedName = "ADMIN"});
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            { 
+                Id = AdminUserId, 
+                UserName = "admin@cognizant.com", 
+                PasswordHash = hasher.HashPassword(null, "Hell0!"), 
+                Email = "admin@cognizant.com", 
+                NormalizedUserName = "admin@cognizant.com".ToUpper(),
+                NormalizedEmail = "admin@cognizant.com".ToUpper(),
+                EmailConfirmed = true
+            });
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { RoleId = AdminRoleId, UserId = AdminUserId });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Ravid@cognizant.com",
+                PasswordHash = hasher.HashPassword(null, "Hell0!"),
+                Email = "Ravid@cognizant.com",
+                NormalizedUserName = "Ravid@cognizant.com".ToUpper(),
+                NormalizedEmail = "Ravid@cognizant.com".ToUpper(),
+                EmailConfirmed = true,
+            });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Edward@cognizant.com",
+                PasswordHash = hasher.HashPassword(null, "Hell0!"),
+                Email = "Edward@cognizant.com",
+                NormalizedUserName = "Edward@cognizant.com".ToUpper(),
+                NormalizedEmail = "Edward@cognizant.com".ToUpper(),
+                EmailConfirmed = true,
+            });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Jessica@cognizant.com",
+                PasswordHash = hasher.HashPassword(null, "Hell0!"),
+                Email = "Jessica@cognizant.com",
+                NormalizedUserName = "Jessica@cognizant.com".ToUpper(),
+                NormalizedEmail = "Jessica@cognizant.com".ToUpper(),
+                EmailConfirmed = true,
+            });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Charles@cognizant.com",
+                PasswordHash = hasher.HashPassword(null, "Hell0!"),
+                Email = "Charles@cognizant.com",
+                NormalizedUserName = "Charles@cognizant.com".ToUpper(),
+                NormalizedEmail = "Charles@cognizant.com".ToUpper(),
+                EmailConfirmed = true,
+            });
+
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Scott@cognizant.com",
+                PasswordHash = hasher.HashPassword(null, "Hell0!"),
+                Email = "Scott@",
+                NormalizedUserName = "Scott@cognizant.com".ToUpper(),
+                NormalizedEmail = "Scott@cognizant.com".ToUpper(),
+                EmailConfirmed = true,
+            });
+
+            base.OnModelCreating(builder);
         }
 
     }
