f5eccf466a30be6fcea84de40bfa09d51683fa2b
Edward Sitar <edwardsitar4@gmail.com>
2023-04-04 08:18:18 -0600

    Add unit test for AdminAPIService

---
+++ health_index_app/Test_health_index_app/Services/AdminAPIServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using health_index_app.Client.Services;
+using RichardSzalay.MockHttp;
+using System;
+using System.Threading.Tasks;
+
+namespace Test_health_index_app.Services
+{
+    public class AdminAPIServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task PostAdminUser_AddsUserSuccessfully()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            string testResponse = @"{ }";
+            var userId = "1234567";
+
+            mockHttp.When("https://localhost:7005/api/admin/adduser")
+                .Respond("application/json", testResponse);
+
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost:7005");
+            var adminAPIService = new AdminAPIServices(client);
+
+            var result = await adminAPIService.PostAdminUser(userId);
+
+            Assert.That(result.StartsWith("StatusCode: 200"));
+            //Assert.That(result.ToString(), Is.EqualTo($"Successfully added user {userId} to admin role")
+
+
+
+        }
+
+    }
+}
