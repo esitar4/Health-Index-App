6ee90947353e3ded866e3b95c29a62b4e07000a3
Edward Sitar <edwardsitar4@gmail.com>
2023-04-05 11:30:21 -0600

    Fix tests, add tests cases for fun

--- health_index_app/Test_health_index_app/Services/FoodAPIServiceTest.cs
+++ health_index_app/Test_health_index_app/Services/FoodAPIServiceTest.cs
@@ -4,6 +4,7 @@ using RichardSzalay.MockHttp;
 using System;
 using System.Threading.Tasks;
 using health_index_app.Shared.Models;
+using Newtonsoft.Json;
 
 namespace Test_health_index_app.Services
 {
@@ -20,9 +21,40 @@ namespace Test_health_index_app.Services
         [TestCase("7654321")]
         public async Task CreateFood_Success(int foodId)
         {
+            Food food = new Food()
+            {
+                Id = foodId,
+                FoodName = "Double-Double Burger",
+                FoodType = "Brand",
+                BrandName = "In-N-Out",
+                FoodURL = "https://www.fatsecret.com/calories-nutrition/in-n-out/double-double-burger",
+                ServingId = 66486,
+                ServingDescription = "1 burger",
+                ServingURL = "https://www.fatsecret.com/calories-nutrition/in-n-out/double-double-burger",
+                MetricServingAmount = 330,
+                MetricServingUnit = "g",
+                NumberOfUnits = 0,
+                MeasurementDescription = "serving",
+                Calories = 670,
+                CarboHydrate = 39,
+                Protein = 37,
+                Fat = 39,
+                SaturatedFat = 17,
+                PolyunsaturatedFat = 0,
+                Cholesterol = 120,
+                Sodium = 1520,
+                Potassium = 0,
+                Fiber = 3,
+                Sugar = 10,
+                VitaminD = 0,
+                VitaminA = 0,
+                VitaminC = 0,
+                Calcium = 0,
+                Iron = 0
+            };
             var mockHttp = new MockHttpMessageHandler();
-            string testResponse = @"{ }";
-            mockHttp.When("https://localhost:7005//food/create")
+            string testResponse = JsonConvert.SerializeObject(food);
+            mockHttp.When("https://localhost:7005/food/create")
                     .Respond("application/json", testResponse);
 
             var client = mockHttp.ToHttpClient();
@@ -33,15 +65,47 @@ namespace Test_health_index_app.Services
             var result = await foodAPIService.CreateFood(foodId);
 
             //Assert
-            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(foodId));
         }
 
         [Test]
-        public async Task ReadFood_Success(int foodId)
+        public async Task ReadFood_Success()
         {
+            Food food = new Food()
+            {
+                Id = 26547,
+                FoodName = "Double-Double Burger",
+                FoodType = "Brand",
+                BrandName = "In-N-Out",
+                FoodURL = "https://www.fatsecret.com/calories-nutrition/in-n-out/double-double-burger",
+                ServingId = 66486,
+                ServingDescription = "1 burger",
+                ServingURL = "https://www.fatsecret.com/calories-nutrition/in-n-out/double-double-burger",
+                MetricServingAmount = 330,
+                MetricServingUnit = "g",
+                NumberOfUnits = 0,
+                MeasurementDescription = "serving",
+                Calories = 670,
+                CarboHydrate = 39,
+                Protein = 37,
+                Fat = 39,
+                SaturatedFat = 17,
+                PolyunsaturatedFat = 0,
+                Cholesterol = 120,
+                Sodium = 1520,
+                Potassium = 0,
+                Fiber = 3,
+                Sugar = 10,
+                VitaminD = 0,
+                VitaminA = 0,
+                VitaminC = 0,
+                Calcium = 0,
+                Iron = 0
+            };
+
             var mockHttp = new MockHttpMessageHandler();
-            string testResponse = @"{ }";
-            mockHttp.When("https://localhost:7005//food/read?FoodId=1234")
+            string testResponse = JsonConvert.SerializeObject(food);
+            mockHttp.When("https://localhost:7005/food/read?FoodId=26547")
                     .Respond("application/json", testResponse);
 
             var client = mockHttp.ToHttpClient();
@@ -49,10 +113,10 @@ namespace Test_health_index_app.Services
             var foodAPIService = new FoodAPIServices(client);
 
             //Act
-            var result = await foodAPIService.CreateFood(foodId);
+            var result = await foodAPIService.ReadFood(26547);
 
             //Assert
-            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Id, Is.EqualTo(26547));
         }
 
         [Test]
