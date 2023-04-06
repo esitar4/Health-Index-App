c132336d92199a6b26d2d12e8974f2b14a5b036e
Edward Sitar <edwardsitar4@gmail.com>
2023-04-06 09:27:36 -0600

    Update unit tests (#70)
    * Update unit tests
    
    * Update unit tests

--- health_index_app/Test_health_index_app/Services/MealFoodAPIServiceTest.cs
+++ health_index_app/Test_health_index_app/Services/MealFoodAPIServiceTest.cs
@@ -98,8 +98,9 @@ namespace Test_health_index_app.Services
             };
 
             var mockHttp = new MockHttpMessageHandler();
-            string testResponse = @"{ }";
-            mockHttp.When("https://localhost:7005//mealfood/update")
+            string testResponse = "StatusCode: 200, ReasonPhrase: 'OK', Version: 1.1, Content: System.Net.Http.BrowserHttpHandler+BrowserHttpContent, Headers:\n{\n Access-Control-Allow-Origin: *\n Date: Tue, 04 Apr 2023 22:05:32 GMT\n Server: Kestrel\n Content-Type: text/plain; charset=utf-8\n}";
+
+            mockHttp.When("https://localhost:7005/mealfood/update")
                     .Respond("application/json", testResponse);
 
             var client = mockHttp.ToHttpClient();
@@ -121,6 +122,40 @@ namespace Test_health_index_app.Services
         [TestCase(1234567)]
         [TestCase(7654321)]
         [TestCase(7654321 - 1234567)]
+        public async Task UpdateMealFood_Fail(int id)
+        {
+            MealFood mealFood = new MealFood()
+            {
+                Id = id,
+                Meal = new Meal(),
+                Food = new Food(),
+                Amount = 1.0
+            };
+
+            var mockHttp = new MockHttpMessageHandler();
+            string testResponse = @"false";
+            mockHttp.When("https://localhost:7005//mealfood/update")
+                    .Respond("application/json", testResponse);
+
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost:7005/");
+            var mealFoodAPIService = new MealFoodAPIServices(client);
+
+            //Act
+            var result = await mealFoodAPIService.UpdateMealFood(mealFood);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        [TestCase(0000)]
+        [TestCase(1000)]
+        [TestCase(1001)]
+        [TestCase(100 - 001)]
+        [TestCase(1234567)]
+        [TestCase(7654321)]
+        [TestCase(7654321 - 1234567)]
         public async Task DeleteMealFood_Success(int id)
         {
             MealFood mealFood = new MealFood()
@@ -132,8 +167,8 @@ namespace Test_health_index_app.Services
             };
 
             var mockHttp = new MockHttpMessageHandler();
-            string testResponse = @"{ }";
-            mockHttp.When("https://localhost:7005//mealfood/delete")
+            string testResponse = "StatusCode: 200, ReasonPhrase: 'OK', Version: 1.1, Content: System.Net.Http.BrowserHttpHandler+BrowserHttpContent, Headers:\n{\n Access-Control-Allow-Origin: *\n Date: Tue, 04 Apr 2023 22:05:32 GMT\n Server: Kestrel\n Content-Type: text/plain; charset=utf-8\n}";
+            mockHttp.When("https://localhost:7005/mealfood/delete")
                     .Respond("application/json", testResponse);
 
             var client = mockHttp.ToHttpClient();
@@ -145,6 +180,41 @@ namespace Test_health_index_app.Services
 
             //Assert
             Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        [TestCase(0000)]
+        [TestCase(1000)]
+        [TestCase(1001)]
+        [TestCase(100 - 001)]
+        [TestCase(1234567)]
+        [TestCase(7654321)]
+        [TestCase(7654321 - 1234567)]
+        public async Task DeleteMealFood_Fail(int id)
+        {
+            MealFood mealFood = new MealFood()
+            {
+                Id = id,
+                Meal = new Meal(),
+                Food = new Food(),
+                Amount = 1.0
+            };
+
+            var mockHttp = new MockHttpMessageHandler();
+            string testResponse = @"false";
+            mockHttp.When("https://localhost:7005//mealfood/delete")
+                    .Respond("application/json", testResponse);
+
+            var client = mockHttp.ToHttpClient();
+            client.BaseAddress = new Uri("https://localhost:7005/");
+            var mealFoodAPIService = new MealFoodAPIServices(client);
+
+            //Act
+            var result = await mealFoodAPIService.DeleteMealFood(mealFood);
+
+            //Assert
+            Assert.IsFalse(result);
         }
     }
 }
