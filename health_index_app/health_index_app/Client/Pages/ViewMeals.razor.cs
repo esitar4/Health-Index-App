0ae7eb6b7a548f6d1b0c1d5d3f9d35ac131dece7
Scott Li <vikash23galce@gmail.com>
2023-04-17 23:34:29 -0600

    Update Index page sizing issue and create view meals template

---
+++ health_index_app/health_index_app/Client/Pages/ViewMeals.razor.cs
@@ -0,0 +1,28 @@
+﻿using health_index_app.Client.Services;
+using health_index_app.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace health_index_app.Client.Pages
+{
+    partial class ViewMeals
+    {
+        [Inject]
+        IMealsAPIServices MealAPIService { get; set; }
+        
+        [Parameter]
+        public int MealId { get; set; }
+
+        List<Meal> Meals { get; set; } = new List<Meal>();
+
+        public Meal CurMeal { get; set; } = new Meal();
+
+
+        public async void GetMeal(int mealId)
+        {
+            if (mealId != 0) {
+                CurMeal = await MealAPIService.ReadMeal(mealId);
+            }
+        }
+
+    }
+}
