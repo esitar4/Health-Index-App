b5d06f1b2f8aa8601f17f0140da978f6eaddcdce
clmisback <60322787+clmisback@users.noreply.github.com>
2023-07-06 15:29:23 -0600

    Added collapsing to items when removing them

--- health_index_app/health_index_app/Client/wwwroot/js/Calendar.js
+++ health_index_app/health_index_app/Client/wwwroot/js/Calendar.js
@@ -5,6 +5,21 @@
 function drag(ev) {
     ev.dataTransfer.setData("text/html", ev.target.id);
 }
+
+function startDrag(ev) {
+    ev.dataTransfer.setData("text/html", ev.target.id);
+    let element = ev.target;
+
+    element.classList.add('hide');
+}
+
+function endDrag(ev) {
+    let element = ev.srcElement;
+
+    
+
+    element.classList.remove('hide');
+}
 /*
 function drop(ev) {
     ev.preventDefault();
@@ -14,22 +29,46 @@ function drop(ev) {
 */
 function drop(ev, el, day) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text/html");
-    var element = document.getElementById(data);
+    var oldID = ev.dataTransfer.getData("text/html");
+    var element = document.getElementById(oldID);
+    var oldDivID = "";
+    alert("id: " + oldID);
     var newID = element.textContent + "-" + day;
-    alert("data: " + data);
+    //alert("data: " + data);
 
-    if (~data.indexOf("menu")) {
+    if (oldID.includes("menu-draggable")) {
         element = element.cloneNode(true);
+    } else {
+        oldDivID = element.parentElement.id;
     }
+
     element.id = newID;
-    element.ondrop = "";
+    element.ondragstart = startDrag;
+    element.ondragend = endDrag;
     var elemDiv = document.createElement('div');
 
     var i = el.childNodes.length;
-    alert(el.childNodes.length);
-    elemDiv.className = "meal-" + i; elemDiv.id = "day-" + i;
+    //alert(el.childNodes.length);
+    elemDiv.className = "meal-" + i; elemDiv.id = "day-" + day + "-" + elemDiv.className;
     elemDiv.appendChild(element);
+
+    if (oldDivID != "") {
+        var NextDivID = oldDivID.slice(0, oldDivID.length - 1) + (Number(oldDivID.slice(-1)) + 1);
+        var moved = false;
+        console.log("NextDivID: " + NextDivID);
+        console.log("OldDivID: " + oldDivID);
+        console.log("j: " + j + " i:" + i + " has children: " + document.getElementById(NextDivID).hasChildNodes());
+        for (var j = Number(oldDivID.slice(-1)); j <= i && document.getElementById(NextDivID).hasChildNodes(); j++) {
+            NextDivID = oldDivID.slice(0, oldDivID.length - 1) + "" + (j + 1);
+            console.log(document.getElementById(NextDivID));
+            document.getElementById(oldDivID).appendChild(document.getElementById(NextDivID).firstChild);
+            moved = true;
+        }
+        if (moved) {
+            document.getElementById(NextDivID).remove();
+        }
+
+    }
 
     el.appendChild(elemDiv);
 }
@@ -38,5 +77,4 @@ function dropDelete(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text/html");
     document.getElementById(data).remove();
-  
 }
