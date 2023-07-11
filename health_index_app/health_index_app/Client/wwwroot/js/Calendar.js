161a51921ec841a62237f7f0427f70e22605a1a6
clmisback <60322787+clmisback@users.noreply.github.com>
2023-07-11 16:17:07 -0600

    Finished basic drag and drop functionality

--- health_index_app/health_index_app/Client/wwwroot/js/Calendar.js
+++ health_index_app/health_index_app/Client/wwwroot/js/Calendar.js
@@ -1,9 +1,19 @@
-﻿function allowDrop(ev) {
+﻿function allowDrop(ev, el) {
     ev.preventDefault();
 }
 
 function drag(ev) {
     ev.dataTransfer.setData("text/html", ev.target.id);
+}
+
+function dragEnter(ev, el) {
+    ev.preventDefault();
+    el.parentElement.classList.add("hover");
+}
+
+function dragLeave(ev, el) {
+    ev.preventDefault();
+    el.parentElement.classList.remove("hover");
 }
 
 function startDrag(ev) {
@@ -16,8 +26,6 @@ function startDrag(ev) {
 function endDrag(ev) {
     let element = ev.srcElement;
 
-    
-
     element.classList.remove('hide');
 }
 /*
@@ -27,12 +35,49 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 */
-function drop(ev, el, day) {
+
+function dropHere(ev, el) {
+    alert("dropHere");
     ev.preventDefault();
+    console.log(el);
+    var numMeals = el.parentElement.childNodes.length - 1;
+
+    if (numMeals > 5) {
+        return;
+    }
+
     var oldID = ev.dataTransfer.getData("text/html");
     var element = document.getElementById(oldID);
+
+    var elemDiv = document.createElement('div');
+
+    elemDiv.className = "meal-" + numMeals; elemDiv.id = "day-" + el.id.splice(-2) + "-" + elemDiv.className;
+
+    for (var i = numMeals; i >= el.id.slice(-2); i--) {
+        thisElement = document.getElementById(oldID);
+        prevElement = document.getElementById(oldID.splice(0,-2))
+    }
+
+    el.appendChild(elemDiv);
+}
+
+function drop(ev, el) {
+    ev.preventDefault();
+    el.parentElement.classList.remove("hover");
+
+    var oldID = ev.dataTransfer.getData("text/html");
+    var element = document.getElementById(oldID);
+
+    console.log("element.parentElement.id: " + element.parentElement.parentElement.id + " el.id: " + el.id)
+    if (element.parentElement.parentElement.id == el.id) {
+        return;
+    }
+
     var oldDivID = "";
-    alert("id: " + oldID);
+    var numMealsEv = 0;
+    var numMealsEl = el.childNodes.length;
+    var day = el.id.slice(4);
+    //alert("id: " + oldID);
     var newID = element.textContent + "-" + day;
     //alert("data: " + data);
 
@@ -40,34 +85,45 @@ function drop(ev, el, day) {
         element = element.cloneNode(true);
     } else {
         oldDivID = element.parentElement.id;
+        console.log(element);
+        numMealsEv = element.parentElement.parentElement.childNodes.length -1;
     }
 
     element.id = newID;
     element.ondragstart = startDrag;
     element.ondragend = endDrag;
+    element.ondrop = dropHere;
+    element.classList.remove("py-3");
+
     var elemDiv = document.createElement('div');
 
-    var i = el.childNodes.length;
-    //alert(el.childNodes.length);
-    elemDiv.className = "meal-" + i; elemDiv.id = "day-" + day + "-" + elemDiv.className;
+    console.log("numMealsEl: " + numMealsEl + " numMealsEv: " + numMealsEv);
+    
+    elemDiv.className = "meal-" + numMealsEl ; elemDiv.id = "day-" + day + "-" + elemDiv.className;
     elemDiv.appendChild(element);
 
-    if (oldDivID != "") {
+    console.log("OldDivID: " + oldDivID);
+    if (numMealsEv != oldDivID.slice(-1)) {
         var NextDivID = oldDivID.slice(0, oldDivID.length - 1) + (Number(oldDivID.slice(-1)) + 1);
         var moved = false;
-        console.log("NextDivID: " + NextDivID);
-        console.log("OldDivID: " + oldDivID);
-        console.log("j: " + j + " i:" + i + " has children: " + document.getElementById(NextDivID).hasChildNodes());
-        for (var j = Number(oldDivID.slice(-1)); j <= i && document.getElementById(NextDivID).hasChildNodes(); j++) {
-            NextDivID = oldDivID.slice(0, oldDivID.length - 1) + "" + (j + 1);
-            console.log(document.getElementById(NextDivID));
+
+        for (var j = Number(oldDivID.slice(-1)); j <= numMealsEv && document.getElementById(NextDivID).hasChildNodes(); j++) {
+            //console.log(document.getElementById(NextDivID));
             document.getElementById(oldDivID).appendChild(document.getElementById(NextDivID).firstChild);
             moved = true;
+            //console.log("ev.target.parentElement.parentElement.childNodes.length: " + ev.target.parentElement.parentElement.childNodes.length);
+            if (numMealsEv == j + 1) {
+                break;
+            }
+            oldDivID = NextDivID;
+            NextDivID = oldDivID.slice(0, oldDivID.length - 1) + "" + (j + 2);
         }
         if (moved) {
             document.getElementById(NextDivID).remove();
         }
 
+    } else if (oldDivID) {
+        document.getElementById(oldDivID).remove();
     }
 
     el.appendChild(elemDiv);
